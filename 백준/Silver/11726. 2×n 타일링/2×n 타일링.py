7d2e5f32100a27861675a027f6686f026a397620
Soo bin <89141681+dahlia0916@users.noreply.github.com>
2023-01-27 10:57:09 +0900

    [Silver III] Title: 2×n 타일링, Time: 244 ms, Memory: 121788 KB -BaekjoonHub

--- 백준/Silver/11726. 2×n 타일링/2×n 타일링.py
+++ 백준/Silver/11726. 2×n 타일링/2×n 타일링.py
@@ -3,6 +3,11 @@ import math
 n=int(sys.stdin.readline())
 if n ==1:
     print(1)
+elif n % 2 == 0:
+    sol=2
+    for i in range(1,(n//2)):#2가 1개부터 최대개수까지 쓰일때
+        sol=sol+(math.factorial(i+(n-(i*2)))//(math.factorial(i)*math.factorial((n-(i*2)))))
+    print(sol%10007)
 else:
     sol=1
     for i in range(1,(n//2)+1):#2가 1개부터 최대개수까지 쓰일때
