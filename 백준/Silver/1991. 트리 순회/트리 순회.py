30251d045eda6b10dab8fbf56bd68135b686204d
Soo bin <89141681+dahlia0916@users.noreply.github.com>
2023-01-30 22:21:37 +0900

    [Silver I] Title: 트리 순회, Time: 64 ms, Memory: 30616 KB -BaekjoonHub

---
+++ 백준/Silver/1991. 트리 순회/트리 순회.py
@@ -0,0 +1,27 @@
+import sys
+n=int(sys.stdin.readline())
+tree={}
+for i in range(n):
+    nd,left,right = sys.stdin.readline().strip().split()
+    tree[nd]=[left,right]
+def preorder(node):
+    if node != ".":
+        print(node,end="")
+        preorder(tree[node][0])
+        preorder(tree[node][1])
+def inorder(node):
+    if node != ".":
+        inorder(tree[node][0])
+        print(node,end="")
+        inorder(tree[node][1])
+def postorder(node):
+    if node != ".":
+        postorder(tree[node][0])
+        postorder(tree[node][1])
+        print(node,end="")
+
+preorder("A")
+print()
+inorder("A")
+print()
+postorder("A")
