7af4a46cad06984a92b39b2824786082a88720d3
Soo bin <89141681+dahlia0916@users.noreply.github.com>
2023-02-21 09:40:19 +0900

    [Silver II] Title: 유기농 배추, Time: 68 ms, Memory: 31652 KB -BaekjoonHub

---
+++ 백준/Silver/1012. 유기농 배추/유기농 배추.py
@@ -0,0 +1,30 @@
+import sys
+sys.setrecursionlimit(10000)
+def dfs(y,x):
+    v[y][x]=1
+    dy=[-1,0,1,0]
+    dx=[0,1,0,-1]
+    for i in range(4):
+        ny=y+dy[i]
+        nx=x+dx[i]
+        if 0<=ny<N and 0<=nx<M and l[ny][nx]==1 and v[ny][nx]==0:
+            dfs(ny,nx)
+
+T=int(sys.stdin.readline())
+s_l=[]
+for i in range(T):
+    s=0
+    M,N,K=map(int,sys.stdin.readline().split())
+    l=[[0]*M for _ in range(N)]
+    v=[[0]*M for _ in range(N)]
+    for j in range(K):
+        ipx,ipy=map(int,sys.stdin.readline().split())
+        l[ipy][ipx]=1
+    for i2 in range(N):
+        for j2 in range(M):
+            if l[i2][j2]==1 and v[i2][j2] == 0:
+                dfs(i2,j2)
+                s=s+1
+    s_l.append(s)
+for i in range(len(s_l)):
+    print(s_l[i])
